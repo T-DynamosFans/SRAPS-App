e566106dcb3222b46c01fa035b0200cb3a815c3f
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-01 22:17:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-	
 
 
 __version__ = "1.0.0"
 
 
-from kvdroid.tools import get_resource
 from kivymd.app import *
 from kivymd.uix.label import *
 from kivy.uix.image import *
+from kivy.uix.label import *
 from kivy.uix.screenmanager import *
 from kivy.lang import Builder
 from kivy.core.text import LabelBase 
@@ -26,6 +25,7 @@ from kivymd.uix.dialog import MDDialog
 from kivy.utils import get_color_from_hex
 from kivy.animation import Animation
 from kivy.uix.carousel import Carousel
+from kivymd.uix.expansionpanel import *
 import requests
 import webbrowser
 import os
@@ -45,12 +45,12 @@ import _thread
 
 screen_manager = ScreenManager()
 if platform != "android":
-	Window.size = (540,960)
+	Window.size =(360,640)
 
 def check_intr():
 	import requests
 	try:
-		requests.get("https://google.com",timeout=0.5)
+		requests.get("https://motherfuckingwebsite.com",timeout=0.2)
 	except Exception as e:
 		print(str(e))
 		return False
@@ -60,7 +60,7 @@ def check_intr():
 def getDb():
 	apiUrl = "https://raw.githubusercontent.com/T-Dynamos/SRAPS-App/main/app.database"
 	try:
-		a = requests.get(apiUrl)
+		a = requests.get(apiUrl,timeout=1)
 		open('dataDb.py', 'wb').write(a.content)
 		import dataDb
 		from dataDb import DataBase
@@ -74,8 +74,11 @@ class SRAPS_APP(MDApp):
 	screen_manager = screen_manager
 	if check_intr() == True:
 		DataBase = getDb()
-		SilderImages = DataBase["SilderImages"]
+		SliderImages = DataBase["SilderImages"]
 		News = DataBase["News"]
+	else:
+		SliderImages=["https://github.githubassets.com/images/mona-loading-default.gif","https://github.githubassets.com/images/mona-loading-default.gif","https://github.githubassets.com/images/mona-loading-default.gif","https://github.githubassets.com/images/mona-loading-default.gif","https://github.githubassets.com/images/mona-loading-default.gif"]
+		News="No Internet"
 	def build(self):
 
 		screen_manager.add_widget(Builder.load_file('main.kv'))
