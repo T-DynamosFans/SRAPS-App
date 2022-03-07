500939d2ca8088a41f5ebb82d787c5b99a96b060
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-07 10:33:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 
 __version__ = "1.0.0"
-
-
+import kivy
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import *
 from kivymd.uix.label import *
 from kivy.uix.image import *
@@ -24,8 +24,12 @@ from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.dialog import MDDialog
 from kivy.utils import get_color_from_hex
 from kivy.animation import Animation
+from kivy.uix.modalview import ModalView
+
 from kivy.uix.carousel import Carousel
 from kivymd.uix.expansionpanel import *
+from kivymd.uix.datatables import MDDataTable
+from kivy.uix.anchorlayout import AnchorLayout
 import requests
 import webbrowser
 import os
@@ -43,10 +47,11 @@ import time
 import _thread
 from kivymd.uix.dialog import MDDialog
 from functools import partial
+from teach import Teachers
 screen_manager = ScreenManager()
 if platform != "android":
 	Window.size =(360,640)
-	
+print(Window.size)
 def check_intr():
 	import requests
 	try:
@@ -83,17 +88,12 @@ def update_data():
 	o.news.text = str(DataBase["News"])		
 	o.nimg.source = DataBase["SliderImages"]
 
-
-dialo = ["yes"]
-	
 def show_message(Ok):
-	if dialo[-1] is "yes":
-		print(dialo)
 		dialog=Snackbar(text="No Internet!",
 		snackbar_x="10dp",
 
 		radius=[30,30,30,30],
-		snackbar_y="10dp",
+		snackbar_y="55dp",
 		size_hint_x=.95)
 		a = lambda self : (Toast("Updating data"),_thread.start_new_thread(update_data,()),dialog.dismiss())
 		b = lambda self : Toast("Internet not connected")
@@ -111,10 +111,53 @@ def show_message(Ok):
 			text_color=[1,1,1,1],
 			on_press=c)]
 		dialog.auto_dismiss=False
-	dialog.open()
-	
+		dialog.open()
+
+
+def show_teachers(self):
+	card = MDCard(elevation=40, radius=[36,])
+	f = MDDataTable(
+
+	elevation=40,
+	use_pagination=True,
+	column_data=[
+
+	("No.", dp(20)),
+
+	("Name", dp(20)),
+
+	("Designation", dp(20)),
+
+	("Qualification", dp(20))],
+	row_data = Teachers)
+
+	card.add_widget(f)
+	modal = ModalView(
+
+	size_hint=(0.9, 0.9),
+
+	overlay_color=(0, 0, 0, 0),
+
+	)
+
+	modal.add_widget(card)
+
+	modal.open()
+def get_part_of_day(h):
+    return (
+        "Morning"
+        if 5 <= h <= 11
+        else "Afternoon"
+        if 12 <= h <= 17
+        else "Evening"
+        if 18 <= h <= 22
+        else "Night"
+    )
+from datetime import datetime
 
 class SRAPS_APP(MDApp):
+	spacing = Window.size[1]//20
+	time = get_part_of_day(datetime.now().hour)
 	screen_manager = screen_manager
 	NI="assets/no-internet.png"
 	News="No Internet"
@@ -132,5 +175,7 @@ class SRAPS_APP(MDApp):
 			show_message("true")
 
 		os.system("rm assets/tmp-* ")
+	def table(self):
+		show_teachers("hy")
 
 SRAPS_APP().run()
