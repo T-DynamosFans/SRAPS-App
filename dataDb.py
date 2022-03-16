8451c148d8241dd7dbf23d3f388dcb125fbd177f
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-16 10:42:28 +0530

    Update dataDb.py

--- dataDb.py
+++ dataDb.py
@@ -138,3 +138,15 @@ Teachers = (
 ['130', ' Mr.Viney Chanana', 'Teacher', 'Btech(Ece), Msc(Physics)'],
 ['131', ' Mr.Uttam Dash', 'Teacher', 'Matric']
 )
+links = ["http://www.shriramashramps.org/assets/img/clogo/1.png",
+"http://www.shriramashramps.org/assets/img/clogo/2.png",
+"http://www.shriramashramps.org/assets/img/clogo/3.png",
+"http://www.shriramashramps.org/assets/img/clogo/4.png",
+"http://www.shriramashramps.org/assets/img/clogo/5.png",
+"http://www.shriramashramps.org/assets/img/clogo/6.png",
+"http://www.shriramashramps.org/assets/img/clogo/7.png",
+"http://www.shriramashramps.org/assets/img/clogo/8.png",
+"http://www.shriramashramps.org/assets/img/clogo/9.png",
+"http://www.shriramashramps.org/assets/img/clogo/10.png",
+"http://www.shriramashramps.org/assets/img/clogo/11.png",
+"http://www.shriramashramps.org/assets/img/clogo/12.png"]
